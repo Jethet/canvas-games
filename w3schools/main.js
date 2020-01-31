d3387dfe34f1d50cfdac9b6db827e844efd2320a
Jethet <henriette.hettinga@gmail.com>
2020-01-31 13:48:20 +0100

    Start drawing and styling on canvas

--- w3schools/main.js
+++ w3schools/main.js
@@ -1,4 +1,10 @@
-var canvas = document.getElementById("myCanvas");
+var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
+ctx.fillStyle = "#purple";
 ctx.fillRect(0, 0, 150, 75);
+
+ctx.fillStyle = 'rgb(200, 0, 0)';
+ctx.fillRect(10, 10, 50, 50);
+
+ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+ctx.fillRect(30, 30, 50, 50);
